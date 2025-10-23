2de71d99a02f5598b67f22b59db1ef58ef5afee8
LucasOleques <lucasoleques1903@gmail.com>
2025-10-23 01:23:24 -0300

    feat: adiciona autenticação por token e configurações de permissão nas views de contas

--- fin_track_project/apps/accounts/views.py
+++ fin_track_project/apps/accounts/views.py
@@ -2,10 +2,32 @@ from rest_framework import viewsets
 from .models import Account
 from .serializer import AccountSerializer
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
 
 class AccountViewSet(viewsets.ModelViewSet):
     queryset = Account.objects.all()
     serializer_class = AccountSerializer
     
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
     filter_backends = [DjangoFilterBackend]
-    filterset_fields = ['name_bank', 'status', 'category'] # Neste campo coloque somente.
+    filterset_fields = ['name_bank', 'status', 'category']
+
+    def get_queryset(self):
+        return super().get_queryset().order_by('id')
+    
+    def get_serializer_context(self):
+        context = super().get_serializer_context()
+        context.update({"request": self.request})
+        return context
+    
+    def perform_create(self, serializer):
+        serializer.save()
+
+    def perform_update(self, serializer):
+        serializer.save()
+
+    def perform_destroy(self, instance):
+        instance.delete()
