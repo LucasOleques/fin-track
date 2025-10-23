e008c7d5b5c4603ed315f9bf70ddc4ad4bfbd275
LucasOleques <lucasoleques1903@gmail.com>
2025-10-23 01:22:46 -0300

    fix: corrige a rota de usuários para 'user' e adiciona a rota de token de autenticação

--- fin_track_project/fin_track_project/urls.py
+++ fin_track_project/fin_track_project/urls.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('users/', include('users.urls', namespace='users')),
+    path('user/', include('user.urls', namespace='user')),
     path('accounts/', include('accounts.urls', namespace='accounts')),
     path('transactions/', include('transactions.urls', namespace='transactions')),
     path('categories/', include('categories.urls', namespace='categories')),
+    path('token-autenticacao/', obtain_auth_token),
 ]
